90dd67bccf06467ab30e06fa3dea518eb9e67643
Eric Su <esericsu@gmail.com>
2020-07-14 00:35:43 -0700

    add cassandr seed scripts

--- cassandra/seedPhotos.js
+++ cassandra/seedPhotos.js
@@ -1,18 +1,52 @@
 /* eslint-disable prefer-destructuring */
 /* eslint-disable camelcase */
 const fs = require('fs');
-// const faker = require('faker');
-// const TimeUuid = require('cassandra-driver').types.TimeUuid;
-// const csvWriter = require('csv-write-stream');
 const path = require('path');
-
-// const writer = csvWriter();
-// const { getPhotos } = require('./sdk');
-const { roomsData, generateRoomsData } = require('./normalData');
 
 const filePath = path.join(__dirname, '../csv/cassphotos1.csv');
 const writePhotos = fs.createWriteStream(filePath);
 writePhotos.write('room_id,photo_id,photo_url,photo_description,photo_name,photo_order\n', 'utf8');
+
+const { photosUrls } = require('./photosUrls.js');
+
+const roomRecords = 1000001;
+//change this to N number of records
+
+//normal data
+const generatePhotosData = (urls) => {
+  const newUrls = Array.from(urls);
+  const photosData = [];
+  const urlsData = [];
+  const randomNPhotos = Math.floor(Math.random() * (8 - 5 + 1)) + 5;
+  for (let i = 0; i < randomNPhotos + 1; i++) {
+    urlsData.push(newUrls[i]);
+    newUrls.splice(i, 1);
+    const photoData = {
+      photo_id: TimeUuid.fromDate(faker.date.between('2017-01-01', '2020-07-01')), // get timeuuid between dates
+      photo_url: photosUrls[i],
+      photo_description: `${faker.company.bs()} ${faker.company.bs()}`,
+      photo_name: faker.company.catchPhraseDescriptor(),
+      photo_order: i,
+    };
+    photosData[i] = photoData;
+  }
+  return photosData;
+};
+
+const generateRoomsData = (photosData) => {
+  const roomsData = [];
+  for (let i = 1; i < roomRecords; i++) {
+    const data = {
+      room_id: i,
+      photos: photosData,
+    };
+    roomsData[i - 1] = data;
+  }
+  return roomsData;
+};
+
+const photosData = generatePhotosData(photosUrls);
+const roomsData = generateRoomsData(photosData);
 
 const allPhotos = (roomData) => {
   const result = [];
@@ -36,9 +70,6 @@ const allPhotos = (roomData) => {
   return result;
 };
 
-// const photoDataArray = allPhotos(roomsData);
-// console.log(photoDataArray);
-
 const writeMassivePhotos = (writer, data, callback) => {
   let i = 0;
   let dataIndex = 0;
@@ -46,7 +77,8 @@ const writeMassivePhotos = (writer, data, callback) => {
     let ok = true;
     do {
       if (i % 1000000 === 0) {
-        console.log('wrote 1 MM rows');
+        // writer.once('drain', write);
+        console.log('wrote 1MM')
       }
       i += 1;
       if (i === 0) {
--- cassandra/testdrain.js
+++ cassandra/testdrain.js
@@ -1,20 +1,22 @@
 const faker = require('faker');
 const fs = require('fs');
 const TimeUuid = require('cassandra-driver').types.TimeUuid;
-const writeUsers = fs.createWriteStream('users.csv');
+const writeUsers = fs.createWriteStream('/Users/eric/Documents/HACKR/HRSF128/kinglia-photogallery/csv/testtest.csv');
 writeUsers.write('id,username,avatar\n', 'utf8');
 
 function writeTenMillionUsers(writer, encoding, callback) {
-  let i = 100000;
+  let i = 80000000;
   let id = 0;
   function write() {
     let ok = true;
     do {
+      if (i % 10000000 === 0) {
+        console.log('wrote 10MM');
+      }
       i -= 1;
       id += 1;
       const username = faker.internet.userName();
-      const avatar = faker.image.avatar()
-
+      const avatar = faker.image.avatar();
       const data = `${id},${username},${avatar}\n`;
       if (i === 0) {
         writer.write(data, encoding, callback);
@@ -38,3 +40,4 @@ write()
 writeTenMillionUsers(writeUsers, 'utf-8', () => {
   writeUsers.end();
 });
+
