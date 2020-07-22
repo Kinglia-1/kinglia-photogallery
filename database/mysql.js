7b4ea0865a65333fde7fabb4298594bc4f318ba0
Eric Su <esericsu@gmail.com>
2020-07-22 10:26:41 -0700

    refactor config

--- database/mysql.js
+++ database/mysql.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2');
 const { config } = require('../config.js');
-require('events').EventEmitter.defaultMaxListeners = 100;
+// require('events').EventEmitter.defaultMaxListeners = 100;
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: config.host,
+  user: 'eric',
   password: config.mysqlpass,
   database: 'bnbphotos',
   multipleStatements: true,
+  port: 3306,
 });
 
 module.exports = connection;
